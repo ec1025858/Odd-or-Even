994c3453cda7fb3523c7f88fd6129eedd3bafd89
ec1025858 <ec1025858@otc.edu>
2025-10-30 08:27:27 -0500

    Update Program.cs
    I finished the code

--- Odd or Even/Program.cs
+++ Odd or Even/Program.cs
@@ -1,3 +1,13 @@
 ﻿Console.WriteLine("Type in a number");
 Console.Write(">> ");
 int num = Convert.ToInt32(Console.ReadLine());
+
+int check = num % 2;
+if (check == 1)
+{
+    Console.WriteLine("The number is Odd");
+}
+else
+{
+    Console.WriteLine("The number is Even");
+}
